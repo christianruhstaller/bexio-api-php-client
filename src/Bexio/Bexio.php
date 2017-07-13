d35241038e8fbf56045d83345418157ca71e13e6
Christian Ruhstaller <christian.ruhstaller@goldinteractive.ch>
2017-07-13 11:15:39 +0200

    Add functions

--- src/Bexio/Bexio.php
+++ src/Bexio/Bexio.php
@@ -21,18 +21,55 @@ class Bexio
         return $this->client->get('contact', $params);
     }
 
+    /**
+     * Search for contacts
+     *
+     * @param array $params
+     * @return mixed
+     */
     public function searchContacts(array $params = [])
     {
         return $this->client->get('contact/search', $params);
     }
 
+    /**
+     * Get specific contact
+     *
+     * @param $id
+     * @return mixed
+     */
     public function getContact($id)
     {
         return $this->client->get('contact/search/'.$id, []);
     }
 
+    /**
+     * Get relations from contacts
+     *
+     * @return mixed
+     */
     public function getContactsRelations()
     {
         return $this->client->get('contact_relation', []);
+    }
+
+    /**
+     * Get available salutations
+     *
+     * @return mixed
+     */
+    public function getSalutations()
+    {
+        return $this->client->get('salutation', []);
+    }
+
+    /**
+     * Get available titles
+     *
+     * @return mixed
+     */
+    public function getTitles()
+    {
+        return $this->client->get('title', []);
     }
 }
