325dfd873f84dd8d68cc826bd4ee91c16191297b
Aid Arslanagic <aarsla@simpastudio.com>
2018-03-11 16:57:53 +0100

    pdf, comments, docs

--- src/Bexio/Resource/Item.php
+++ src/Bexio/Resource/Item.php
@@ -4,21 +4,28 @@ namespace Bexio\Resource;
 
 use Bexio\Bexio;
 
+/**
+ * Class Item
+ * @package Bexio\Resource
+ * https://docs.bexio.com/ressources/article/
+ */
 class Item extends Bexio {
 
     /**
      * Gets all items
      *
+     * @param array $params
      * @return array
      */
     public function getItems(array $params = [])
     {
         return $this->client->get('article', $params);
     }
-    
+
     /**
      * Get item types
-     * 
+     *
+     * @param array $params
      * @return array
      */
     public function getItemTypes(array $params = [])
