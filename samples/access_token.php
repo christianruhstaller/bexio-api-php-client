9af77b32663422bad60b5823b373c3e8030fc49f
Herbert Scheffknecht <herbert@xoo.cc>
2021-01-11 10:38:16 +0100

    Pull + merge new commits

--- samples/access_token.php
+++ samples/access_token.php
@@ -16,6 +16,14 @@ $client = new Client();
 $client->setAccessToken($accessToken);
 $client->setJsonDecodeAssoc(true);
 
+$client->onRequest(function (string $requestUrl, array $parameters) {
+    echo sprintf('Requesting %s with %d parameters…' . PHP_EOL, $requestUrl, count($parameters));
+});
+
+$client->onResponse(function (string $requestUrl, string $response) {
+    echo sprintf('Response of %s has %d bytes' . PHP_EOL, $requestUrl, strlen($response));
+});
+
 $bexio = new Bexio($client);
 
 var_dump($bexio->getLanguages());
